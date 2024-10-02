2e75b105e4def735d7887c59aee0f139cbe075a1
Sergio PC <sergiosantosmenaq@gmail.com>
2024-10-01 20:18:52 -0500

    Time middleware

---
+++ app/Http/Middleware/TimeMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Exceptions\OutsideWorkingHours;
+use Carbon\Carbon;
+
+class TimeMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $startTime = Carbon::createFromTime(9, 0, 0);
+        $endTime = Carbon::createFromTime(21, 0, 0);
+    
+        $now = Carbon::now();
+    
+        if ($now->lessThan($startTime) || $now->greaterThan($endTime)) {
+            return response("Access denied. Outside working hours.");
+        }
+    
+        return $next($request);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\ClientController;
+use App\Http\Middleware\TimeMiddleware;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/clients', [ClientController::class, 'index']);
+Route::get('/clients', [ClientController::class, 'index'])->middleware(TimeMiddleware::class);
